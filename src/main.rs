b4cf3f6c36ec6645b8cf1d4b95035594337dcc1c
Felix Dumbeck <felix@dumbeck.net>
2024-07-07 02:38:06 +0200

    decode requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 use proto::{
     vm_runtime_server::{VmRuntime, VmRuntimeServer},
     ExecuteRequest,
@@ -21,21 +23,51 @@ impl VmRuntime for ZokratesService {
         &self,
         request: tonic::Request<proto::CreateRequest>,
     ) -> Result<tonic::Response<proto::CreateResponse>, tonic::Status> {
-        todo!()
+        println!("received create");
+        match convert_zlib_hex_to_string(&request.into_inner().content) {
+            Ok(s) => println!("{}", s),
+            Err(e) => eprintln!("{}", e),
+        };
+
+        let response = proto::CreateResponse {};
+        Ok(tonic::Response::new(response))
     }
+
     async fn execute_operator(
         &self,
         request: tonic::Request<ExecuteRequest>,
     ) -> Result<tonic::Response<proto::ExecuteResponse>, tonic::Status> {
-        todo!()
+        println!("received exe");
+        let response = proto::ExecuteResponse {
+            result: "asdf".into(),
+        };
+        Ok(tonic::Response::new(response))
     }
+}
+
+fn convert_zlib_hex_to_string(hex_str: &str) -> Result<String, String> {
+    let bytes = match hex::decode(hex_str) {
+        Ok(bytes) => bytes,
+        Err(e) => return Err(format!("couldn't convert hex string to bytes {}", e)),
+    };
+
+    let mut d = flate2::read::ZlibDecoder::new(bytes.as_slice());
+
+    let mut s = String::new();
+
+    match d.read_to_string(&mut s) {
+        Ok(_) => {}
+        Err(e) => return Err(format!("couldn't decompress string {}", e)),
+    };
+
+    Ok(s)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:5000".parse()?;
+    let addr = "0.0.0.0:4004".parse()?;
 
-    println!("starting server on {}", addr);
+    println!("starting grpc server on {}", addr);
 
     let zok = ZokratesService::default();
 
