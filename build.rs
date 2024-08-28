b580eee6f7fcc44ec465c6d99db35f40d263a6cf
Felix Dumbeck <felix@dumbeck.net>
2024-08-28 22:20:11 +0200

    just look for PROTOC

--- build.rs
+++ build.rs
@@ -1,18 +1,7 @@
 use std::error::Error;
-use std::{env, path::PathBuf, process::Command};
+use std::{env, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let protoc_path = Command::new("which")
-        .arg("protoc-c")
-        .output()
-        .expect("Failed to execute `which protoc` command");
-
-    let protoc_path = std::str::from_utf8(&protoc_path.stdout)
-        .expect("Failed to convert output to string")
-        .trim();
-
-    env::set_var("PROTOC", protoc_path);
-
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     tonic_build::configure()
